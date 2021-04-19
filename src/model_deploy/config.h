28c22c56ad5fc1c4c3d80dab95537461d67f6a40
tina988988 <tina20001202tw@gmail.com>
2021-04-19 05:54:42 -0700

    add -A

--- src/model_deploy/config.h
+++ src/model_deploy/config.h
@@ -10,7 +10,7 @@ struct Config {
   const int seq_length = 64;
 
   // The number of expected consecutive inferences for each gesture type.
-  const int consecutiveInferenceThresholds[label_num] = {5, 10, 15};
+  const int consecutiveInferenceThresholds[label_num] = {5, 10, 40};
 
   const char* output_message[label_num] = {
         "RING:\n\r"
@@ -30,7 +30,7 @@ struct Config {
         "   *             \n\r"
         "  *              \n\r"
         " * * * * * * * * \n\r"
-        "STOP:\n\r"
+        "VERTICAL:\n\r"
         " *        \n\r"
         " *         \n\r"
         " *          \n\r"
